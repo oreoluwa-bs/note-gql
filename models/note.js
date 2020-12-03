e4419b409e5f53673f7aae0b61460279000f9f17
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-12-03 16:31:11 +0100

    Update note mongoose schema

--- models/note.js
+++ models/note.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
-const { composeWithMongoose } = require("graphql-compose-mongoose");
+const { composeMongoose } = require("graphql-compose-mongoose");
+const { default: slugify } = require("slugify");
 
 const noteSchema = new mongoose.Schema(
   {
+    title: {
+      type: String,
+      default: "Untitled Note",
+    },
+    content: {
+      type: String,
+    },
+    slug: String,
     createdAt: {
       type: Date,
       default: Date.now(),
@@ -14,7 +23,17 @@ const noteSchema = new mongoose.Schema(
   }
 );
 
+// Middlewares
+// eslint-disable-next-line func-names
+noteSchema.pre("save", function (next) {
+  const id = this._id.toString();
+  this.slug = slugify(`${this.title} ${id.slice(id.length - 3)}`, {
+    lower: true,
+  });
+  next();
+});
+
 const Note = mongoose.model("Note", noteSchema);
-const NoteTC = composeWithMongoose(Note);
+const NoteTC = composeMongoose(Note);
 
 module.exports = { Note, NoteTC };
--- schema/note.js
+++ schema/note.js
@@ -1,11 +1,13 @@
 const { NoteTC } = require("../models/note");
 
 const NoteQuery = {
+  getNotes: NoteTC.getResolver("findMany"),
   getNoteById: NoteTC.getResolver("findById"),
 };
 
 const NoteMutation = {
   createNoteOne: NoteTC.getResolver("createOne"),
+  deleteNoteOne: NoteTC.getResolver("removeOne"),
 };
 
 module.exports = { NoteQuery, NoteMutation };
